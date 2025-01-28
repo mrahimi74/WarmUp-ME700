d8a9ef25cdda6fb37c346325053d919605da14da
mrahimi74 <mr-rahimi@outlook.com>
2025-01-28 14:55:24 -0500

    Update bisection.py

--- main_function/bisection.py
+++ main_function/bisection.py
@@ -1,16 +1,15 @@
-
-
+from basic_functions import functions as fn
 
 def bisec(a, b, tol, func):
     if func(a) * func(b) > 0:
         raise ValueError("The function at a and b must have different signs")
     else:
-        c = Avg(a, b)
-        while abs(c - a) > tol or func(c) > tol:
-            if sign(func(c)) == sign(func(a)):
+        c = fn.Avg(a, b)
+        while fn.abs(c - a) > tol or func(c) > tol:
+            if fn.sign(func(c)) == fn.sign(func(a)):
                 a = c
             else:
                 b = c
 
-            c = Avg(a, b)
+            c = fn.Avg(a, b)
         return c
