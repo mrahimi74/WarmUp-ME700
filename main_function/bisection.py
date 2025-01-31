ffbaafd571aed6110986ef33db8cec2b2a6ea91b
mrahimi74 <mr-rahimi@outlook.com>
2025-01-31 18:25:19 -0500

    Update bisection.py

--- main_function/bisection.py
+++ main_function/bisection.py
@@ -1,15 +1,42 @@
-from basic_functions import functions as fn
+def Avg(x, y):
+    return (x + y) / 2
+
+def sign(x):
+    if x > 0:
+        return 1
+    else:
+        return -1
+
+def abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+def check(a: float, b: float):
+    if a >= b:
+        raise ValueError(f"Invalid input: {a} is greater than {b}.")
+    return True
+
+def check_iter(num_iter: int, max_iter: int):
+    if num_iter > max_iter:
+        raise ValueError(f"Maximum number of iterations ({max_iter}) reached without convergence")
+    return
+
+def opps_sign(a, b, func):
+    if func(b) * func(a) < 0:
+        return True
+    else:
+        raise ValueError("f(a) and f(b) must have opposite signs")
 
 def bisec(a, b, tol, func):
-    if func(a) * func(b) > 0:
-        raise ValueError("The function at a and b must have different signs")
-    else:
-        c = fn.Avg(a, b)
-        while fn.abs(c - a) > tol or func(c) > tol:
-            if fn.sign(func(c)) == fn.sign(func(a)):
+    if opps_sign(a, b, func):
+        c = Avg(a, b)
+        while abs(c - a) > tol or func(c) > tol:
+            if sign(func(c)) == sign(func(a)):
                 a = c
             else:
                 b = c
 
-            c = fn.Avg(a, b)
-        return c
+            c = Avg(a, b)
+    return c
