501132b2afc5aca85743b56c4f643c5f63c39c1a
mrahimi74 <mr-rahimi@outlook.com>
2025-01-31 18:34:27 -0500

    Update test_example_functions.py

--- tests/test_example_functions.py
+++ tests/test_example_functions.py
@@ -1,29 +1,44 @@
 import numpy as np
-from basic_functions import functions as fn
+from main_function import bisection as bisc
 
 def test_avg_x_y():
   x = 10
   y = 20
-  assert np.isclose(fn.Avg(x, y), 15)
+  assert np.isclose(bisc.Avg(x, y), 15)
   x = 7
   y = 13
-  assert np.isclose(fn.Avg(x, y), 10)
+  assert np.isclose(bisc.Avg(x, y), 10)
   x = 3
   y = 5
-  assert np.isclose(fn.Avg(x, y), 4)
+  assert np.isclose(bisc.Avg(x, y), 4)
   x = 18
   y = 24
-  assert np.isclose(fn.Avg(x, y), 21)
+  assert np.isclose(bisc.Avg(x, y), 21)
 
 def test_sign():
   x = 12
-  assert np.isclose(fn.sign(x), 1)
+  assert np.isclose(bisc.sign(x), 1)
   x = -4
-  assert np.isclose(fn.sign(x), -1)
+  assert np.isclose(bisc.sign(x), -1)
 
 def test_abs():
   x = 19
-  assert np.isclose(fn.abs(x), 19)
+  assert np.isclose(bisc.abs(x), 19)
   x = -241
-  assert np.isclose(fn.abs(x), 241)
+  assert np.isclose(bisc.abs(x), 241)
   
+def test_check():
+  x = 12
+  y = 17
+  assert np.isclose(bisc.check(x,y, True))
+  x = 9
+  y = 16
+  assert np.isclose(bisc.check(x,y,True))
+
+def fn(x):
+  return x**2 - 14
+
+def test_opps_sign():
+  x = 3
+  y = 5
+  assert np.isclose(bisc.opps_sign(x, y, True))
